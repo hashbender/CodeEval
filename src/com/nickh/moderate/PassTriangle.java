5b494e9cf1f7df9014e57c7c87d641019e346f7b
Nick Hansen <nick.hansen@salesforce.com>
2015-11-25 16:43:03 -0700

    Wrong again, but still not using dynamic programming like a boss.

--- src/com/nickh/moderate/PassTriangle.java
+++ src/com/nickh/moderate/PassTriangle.java
@@ -1,12 +1,7 @@
 package com.nickh.moderate;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 /**
  * 
@@ -15,41 +10,32 @@ import java.util.List;
  */
 public class PassTriangle {
 	public static void main(String[] args) throws IOException {
-		//File file = new File(args[0]);
-		//BufferedReader buffer = new BufferedReader(new FileReader(file));
+		// File file = new File(args[0]);
+		// BufferedReader buffer = new BufferedReader(new FileReader(file));
 		// TODO uncomment this for local testing
-		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(
+				System.in));
 		String line;
 		List<List<Integer>> pyramid = new ArrayList<List<Integer>>();
 		while ((line = buffer.readLine().trim()) != null && line.length() > 0) {
-			List<String> inputStrings = new ArrayList<String>(Arrays.asList(line.split(" ")));
+			List<String> inputStrings = new ArrayList<String>(
+					Arrays.asList(line.split(" ")));
 			List<Integer> input = new ArrayList<Integer>();
 			for (String inputString : inputStrings)
-				
+
 				input.add(Integer.parseInt(inputString));
 			pyramid.add(input);
 		}
-		int sum = 0;
-		int rowAboveIndex = 0;
-		Collections.reverse(pyramid);
-		rowAboveIndex = pyramid.get(0).indexOf(Collections.max(pyramid.get(0)));
-		
-		for (List<Integer> row: pyramid){
-			int left = rowAboveIndex - 1;
-			int right = rowAboveIndex; 
-			if (left < 0) {
-				sum += row.get(right);
-			} else if (row.get(right) > row.get(left)){
-				sum += row.get(right);
-			} else if (row.get(left) > row.get(right)){
-				rowAboveIndex = left;
-				sum += row.get(left);
-			} else if (row.get(left) == row.get(right)){
-				//TODO fuck
-				throw new RuntimeException();
+
+		for (int i = pyramid.size() - 1; i > 0; i--) {
+			for (int j = 0; j < pyramid.get(i).size() - 1; j++) {
+				int number = Math.max(pyramid.get(i).get(j)
+						+ pyramid.get(i - 1).get(j), pyramid.get(i).get(j + 1)
+						+ pyramid.get(i - 1).get(j));
+				pyramid.get(i - 1).set(j, number);
 			}
 		}
-		System.out.println(sum);
+		System.out.println(pyramid.get(0).get(0));
 		buffer.close();
 	}
 }
